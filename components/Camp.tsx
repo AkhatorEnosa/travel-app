60f8c75a982a60691c3447ade50b8cd260c42f48
AkhatorEnosa <osa@Akhators-MacBook-Pro.local>
2024-05-14 18:45:41 +0100

    complete Hero section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Camp = () => {
   return (
     <div>Camp</div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,14 @@
 import Image from 'next/image'
-import React from 'react'
 import Button from './Button'
 
 const Hero = () => {
   return (
-    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
+    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 lg:flex-row'>
       <div className="hero-map"/>
 
       {/* LEFT */}
 
-      <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
+      <div className='relative z-20 flex flex-1 flex-col lg:w-1/2'>
         <Image 
           src="/camp.svg"
           alt='camp'
@@ -55,7 +54,7 @@ const Hero = () => {
       </div>
 
       <div className='relative flex flex-1 items-start'>
-            <div className='relative x-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
+            <div className='relative flex x-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
               <div className='flex flex-col'>
                 <div className="flex flex-col mb-6">
                   <div className='flexBetween'>
