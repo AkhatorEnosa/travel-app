4c910596195721cf9048a4398b631cc3c754684c
AkhatorEnosa <osa@Akhators-MacBook-Pro.local>
2024-05-17 14:23:06 +0100

    Finishing up Guide Section

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,8 +1,58 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Guide = () => {
   return (
-    <div>Guide</div>
+    <section className="flexCenter flex-col">
+      <div className="padding-container max-container w-full pb-24">
+        <Image src="/camp.svg" alt='camp' width={50} height={50}/>
+        <p className='uppercase regular-16 -mt-1 mb-1 text-green-50'> we are here for you</p>
+        <div className='flex flex-wrap justify-between gap-5 lg:gap-10'>
+          <h2 className='capitalize bold-40 lg:bold-64 xl:max-w-[390px]'>guide you to easy paths</h2>
+          <p className='regular-16 text-gray xl:max-w-[520px]'>
+            Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
+        </div>
+      </div>
+
+      <div className='flexCenter max-container relative w-full'>
+        <Image 
+          src="/boat.png"
+          alt='boat'
+          width={1440}
+          height={580}
+          className='w-ful object-cover object-center 2xl:rounded-5xl'
+        />
+
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <Image 
+            src = "/meter.svg"
+            alt='meter'
+            width={16}
+            height={158}
+            className='h-full w-auto'
+          />
+
+          <div className='flexBetween flex-col'>
+            
+            <div className="flex w-full flex-col">
+              <div className='flexBetween w-full'>
+                <p className='regular-16 text-gray-20'>Destination</p>
+                <p className='bold-10 text-green-50'>48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Aguas Calientes</p>
+            </div>
+
+            <div className="flex w-full flex-col">
+              <div className='flexBetween w-full'>
+                <p className='regular-16 text-gray-20'>Start Track</p>          
+              </div>
+              <p className="bold-20 mt-2">Wonorejo Pasuruan</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
